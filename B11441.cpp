a489cbd1544b6504e112165f32c036e1296abe57
Dorimory <limas98@naver.com>
2025-09-12 19:54:53 +0900

    백준

--- B11441.cpp
+++ B11441.cpp
@@ -4,31 +4,51 @@
 #include <cmath>
 
 using namespace std;
-long long NUM[100001];
+long long LEFT[100001];
+long long RIGHT[100001];
+long long CROSS[100001];
 long long SUM[100001];
-
+long long LEFTSUM[100001];
+long long RIGHTSUM[100001];
 int main(){
     ios::sync_with_stdio(false);
     cin.tie(NULL);
-    int N,M,a,b;
-    long long result=0;
+    int N, a;
+        long long result=0;
     cin >> N;
     
     for (int i = 1; i <= N; i++)
     {
-        cin >> NUM[i];
-        SUM[i] = NUM[i] + SUM[i - 1];
+        cin >> CROSS[i];
     }
-    cin >> M;
-    for (int i = 1; i <= M; i++)
+    for (int i = 1; i < N; i++)
     {
-        cin >> a;
-        cin >> b;
-
-        result = SUM[b]-SUM[a-1];
-        cout << result<<"\n";
+        cin >> LEFT[i];
+        LEFTSUM[i] = LEFT[i]+LEFTSUM[i-1];
+    }
+    for (int i = 1; i < N; i++)
+    {
+        cin >> RIGHT[i];
+        RIGHTSUM[i] = RIGHT[i] + RIGHTSUM[i - 1];
+    }
+    for (int i = 1; i <= N; i++){
+        SUM[i] = LEFTSUM[i - 1] + CROSS[i] + RIGHTSUM[N - 1] - RIGHTSUM[i - 1];
     }
     
+    SUM[0] = SUM[1];
+    result = SUM[0];
+    a = 1;
+    for(int i=1;i<=N;i++){
+
+        if (result > SUM[i])
+        {
+            result = SUM[i];
+            a = i;
+        }
+    }
+    cout << a << " " << result;
+
+   
 
 
 
